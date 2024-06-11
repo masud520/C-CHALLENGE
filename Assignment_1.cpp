1c8e7ad7595043a58b38c15dab6ea18bc4dcdeaa
masud520 <masud.abdi@student.moringaschool.com>
2024-06-11 14:10:45 +0300

    added comments

---
+++ Assignment_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//  cout is used to print each line exactly as specified & The endl is used to insert a newline character after each line of output.
+int main() {
+    cout << "Strathmore University Vision" << endl;
+    cout << "......................................................." << endl;
+    cout << "To become a leading outcome-driven entrepreneurial research" << endl;
+    cout << "University by translating our excellence into major contribution" << endl;
+    cout << "to culture, economic well-being, and quality of life." << endl;
+    return 0;
+}
---
+++ Assignment_2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+// In this program:
+
+// \t is used to insert tab spaces for indentation.
+// \" is used to print double quotes within the string.
+int main() {
+    cout << ".....\"Escape Sequence  practice\"....." << endl;
+    cout << "BBIT is a university diploma designed to focus on :" << endl;
+    cout<<"1.Development"<<endl;
+    cout<<"2.Implementation, and"<<endl;
+    cout<<"3.Management of information system"<<endl;
+    return 0;
+}
